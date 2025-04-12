42b8bb4acc7aba85615e0a8f251ce14d93acb7ec
云深 <yunshen4869@gmail.com>
2025-04-12 22:49:54 +0800

    配置远程连接

--- plugins/model/src/lib.rs
+++ plugins/model/src/lib.rs
@@ -1,10 +1,24 @@
-use kovi::PluginBuilder as plugin;
+use std::sync::Arc;
+use kovi::{MsgEvent, PluginBuilder, RuntimeBot};
 
 #[kovi::plugin]
 async fn main() {
-    plugin::on_msg(|event| async move {
-        if event.borrow_text() == Some("hi") {
-            event.reply("hi")
+    let bot_shore = PluginBuilder::get_runtime_bot();
+    let group_message = {
+        let bot = bot_shore.clone();
+        move |event|{
+            let bot = bot.clone();
+            async move{
+                group_message_event(event, bot).await;
+            }
         }
-    });
+    };
+    PluginBuilder::on_group_msg(group_message)
+}
+
+async fn group_message_event(event: Arc<MsgEvent>, bot: Arc<RuntimeBot>){
+    let group_id = event.group_id.unwrap();
+    if let Some(message) = event.borrow_text() {
+        bot.send_group_msg(group_id,message);
+    }
 }
