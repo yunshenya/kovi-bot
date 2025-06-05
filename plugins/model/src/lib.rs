4cd174b0bc4239a189aee97ae4242a257057155e
yunshen <yunshen4869@gmail.com>
2025-06-05 18:35:06 +0800

    增加注册宏

--- plugins/model/src/lib.rs
+++ plugins/model/src/lib.rs
@@ -1,5 +1,5 @@
-use crate::model::{group_message_event, private_message_event};
 use kovi::PluginBuilder;
+use crate::model::{group_message_event, private_message_event};
 
 mod config;
 mod model;
@@ -7,26 +7,10 @@ mod utils;
 
 #[kovi::plugin]
 async fn main() {
-    let bot_shore = PluginBuilder::get_runtime_bot();
-    let group_message = {
-        let bot = bot_shore.clone();
-        move |event| {
-            let bot = bot.clone();
-            async move {
-                group_message_event(event, bot).await;
-            }
-        }
-    };
-
-    let private_message = {
-        let bot = bot_shore.clone();
-        move |event| {
-            let bot = bot.clone();
-            async move {
-                private_message_event(event, bot).await;
-            }
-        }
-    };
+    register_chat_function!{
+        (group_message,group_message_event),
+        (private_message, private_message_event)
+    }
     PluginBuilder::on_group_msg(group_message);
     PluginBuilder::on_private_msg(private_message);
 }
--- plugins/model/src/utils/mod.rs
+++ plugins/model/src/utils/mod.rs
@@ -1,3 +1,21 @@
 mod system_info;
 
 pub use crate::utils::system_info::system_info_get;
+
+
+#[macro_export]
+macro_rules! register_chat_function {
+    ($(($register_name:ident, $function_name:ident)),* $(,)*) => {
+        let bot_shore = kovi::PluginBuilder::get_runtime_bot();
+        $(let $register_name = {
+            let bot = bot_shore.clone();
+            move |event| {
+                let bot = bot.clone();
+                async move {
+                    $function_name(event, bot).await;
+                }
+            }
+        };)*
+    }
+}
+
